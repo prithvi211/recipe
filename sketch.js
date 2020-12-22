fe2eaf8f684336954c585d0626489edd93bd7cd7
prithvi211 <74590365+prithvi211@users.noreply.github.com>
2020-12-22 15:10:50 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  createSprite(400, 200, 10, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  drawSprites();
+}
